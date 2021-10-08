5d1bb2703b9449636dadaed46ea79c423deaf289
stephkri <stephane.krims@hotmail.ca>
2021-10-08 22:24:52 +0000

    Add seeds

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO users (name, email, password) VALUES
+('Logarithm Yuberton', 'ly1982@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Henkel Dog', 'henkel81@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Schwatzkafula Hinterhopf', 'shinterhopf@aol.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Seltzer Man', 'hello@seltzerman.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Le King de Valdor', 'imasexymanwholovesexywoman@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Hebepziboo Longomerton', 'hlongs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Please Do Not Feed The Bear', 'smokey@parks.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Lakentrimo Gadzooks', 'lg1974@aol.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Hooooooooooooooop', 'hooooooooooooooop@hooooooooooooooop.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active) VALUES
+(1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 93061, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', 28142, true),
+(1, 'Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234, 6, 6, 7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', 83680, true),
+(2, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejito Center', 'Genwezuj', 'Newfoundland and Labrador', 44583, true);
+
+INSERT INTO reservations (start_date, end_date, property_id, guest_id) VALUES
+('2018-09-11', '2018,09-26', 2, 3),
+('2019-01-04', '2019-02-01', 2, 4),
+('2021-10-01', '2021-10-14', 1, 5);
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message) VALUES
+(3, 2, 1, 4, 'messages'),
+(4, 2, 2, 3, 'messages'),
+(5, 1, 3, 5, 'messages');
