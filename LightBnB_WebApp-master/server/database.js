d136486329e61a362d84d24564c700982ac177d0
stephkri <stephane.krims@hotmail.ca>
2021-10-16 00:03:18 +0000

    Make the actual deleteReservation function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -240,8 +240,14 @@ exports.updateReservation = updateReservation;
 //  Deletes an existing reservation
 //
 const deleteReservation = function(reservationId) {
-
+  const queryParams = [reservationId];
+  const queryString = `DELETE FROM reservations WHERE id = $1`;
+  return pool.query(queryString, queryParams)
+    .then(() => console.log("Successfully deleted!"))
+    .catch(() => console.error(err));
 }
+
+exports.deleteReservation = deleteReservation;
 
 // Gets an individual reservations
 const getIndividualReservation = function(reservationId) {
